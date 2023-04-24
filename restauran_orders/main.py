096c15389c259e06d6f6278dbd01b22e8df2ad93
RichAllGuti <richjohnall@gmail.com>
2023-04-23 23:22:26 -0600

    Adding more details to the examples.

--- restauran_orders/main.py
+++ restauran_orders/main.py
@@ -2,10 +2,15 @@ from menu_item import Entree, Side, Drink
 from order import Order
 
 def main():
+
+    # Example
     order = Order()
+
+    #
     entree = Entree("Chicken Alfredo", 15.99, "Chicken")
     side = Side("Garlic Bread", 4.99, "Large")
     drink = Drink("Soda", 2.99, "Medium")
+    drink = Drink("cup of wine", 5.50, "300ml")
 
     order.add_item(entree)
     order.add_item(side)
