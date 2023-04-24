bdf6c7638613a6c1a73d41b4231ddcb096f9ba12
RichAllGuti <richjohnall@gmail.com>
2023-04-23 22:38:53 -0600

    updating the order file, adding the billing function. Adding the main file where I write some execution examples.

---
+++ restauran_orders/main.py
@@ -0,0 +1,21 @@
+from menu_item import Entree, Side, Drink
+from order import Order
+
+def main():
+    order = Order()
+    entree = Entree("Chicken Alfredo", 15.99, "Chicken")
+    side = Side("Garlic Bread", 4.99, "Large")
+    drink = Drink("Soda", 2.99, "Medium")
+
+    order.add_item(entree)
+    order.add_item(side)
+    order.add_item(drink)
+
+    order.generate_bill()
+
+    order.remove_item(side)
+
+    order.generate_bill()
+
+if __name__ == "__main__":
+    main()
--- restauran_orders/order.py
+++ restauran_orders/order.py
@@ -22,3 +22,21 @@ class Order:
         for item in self.items:
             total += item.get_price()
         return total
+
+    #
+    def generate_bill(self):
+
+        #
+        print("\n")
+        print("+--------------------------------------------------+")
+        print("|                     Bill                         |")
+        print("+--------------------------------------------------+")
+        
+        for item in self.items:
+            print(f"| {item.get_name() :<20} {item.get_price() :>10} {'|' :>18}")
+        
+        print("+--------------------------------------------------+")
+        print(f"| {'Total:':<20} {self.get_total() :>10} {'|' :>18}")
+        print("+--------------------------------------------------+")
+        print("\n")
+
